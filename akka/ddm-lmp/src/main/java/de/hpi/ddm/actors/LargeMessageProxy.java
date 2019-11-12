37e36b5fe261a2f5997afa3522d86b2fcee31381
Tobias Niedling <tobiasniedling@localhost.localdomain>
2019-11-12 08:19:58 +0100

    Request Handling as shown in akka Documentation working

--- akka/ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ akka/ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -29,6 +29,30 @@ import java.time.Duration;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.ContentTypes;
+import akka.http.javadsl.model.HttpEntities;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.HttpMethods.*;
+import akka.http.javadsl.model.*;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Directives.*;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+import akka.japi.function.Function;
+
 public class LargeMessageProxy extends AbstractLoggingActor {
 
 	////////////////////////
@@ -92,12 +116,51 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		Source<IncomingConnection, CompletionStage<ServerBinding>> serverSource =
 		Http.get(this.context().system()).bind(ConnectHttp.toHost(c.getHost(), 45454), materializer);
 
+		final Function<HttpRequest, HttpResponse> requestHandler =
+		new Function<HttpRequest, HttpResponse>() {
+		  private final HttpResponse NOT_FOUND =
+			HttpResponse.create()
+			  .withStatus(404)
+			  .withEntity("Unknown resource!");
+	  
+	  
+		  @Override
+		  public HttpResponse apply(HttpRequest request) throws Exception {
+			Uri uri = request.getUri();
+			if (request.method() == HttpMethods.GET) {
+			  if (uri.path().equals("/")) {
+				return
+				  HttpResponse.create()
+					.withEntity(ContentTypes.TEXT_HTML_UTF8,
+					  "<html><body>Hello world!</body></html>");
+			  } else if (uri.path().equals("/hello")) {
+				String name = uri.query().get("name").orElse("Mister X");
+	  
+				return
+				  HttpResponse.create()
+					.withEntity("Hello " + name + "!");
+			  } else if (uri.path().equals("/ping")) {
+				return HttpResponse.create().withEntity("PONG!");
+			  } else {
+				return NOT_FOUND;
+			  }
+			} else {
+			  return NOT_FOUND;
+			}
+		  }
+		};
+
 		CompletionStage<ServerBinding> serverBindingFuture =
 		serverSource.to(Sink.foreach(connection -> {
 			System.out.println("Accepted new connection from " + connection.remoteAddress());
-			// ... and then actually handle the connection
+			connection.handleWith(Flow.of(HttpRequest.class).map(requestHandler), materializer);
 			}
 		)).run(materializer);
+
+		
+  /*final CompletionStage<ServerBinding> binding = 
+        http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), 
+        materializer);*/
 
 		// This will definitely fail in a distributed setting if the serialized message is large!
 		// Solution options:
