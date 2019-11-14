26bb404c88365b5460e716b9b1da23b421e12e0f
Tobias Niedling <tobiasniedling@localhost.localdomain>
2019-11-14 15:10:29 +0100

    Removed unused imports

--- akka/ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ akka/ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,91 +1,41 @@
 package de.hpi.ddm.actors;
 
-import java.io.Serializable;
-
+//General
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import akka.actor.ActorSystem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//HTTP
 import akka.stream.*;
 import akka.stream.javadsl.*;
 import akka.http.javadsl.*;
+import akka.http.javadsl.model.*;
+import akka.http.javadsl.model.HttpMethods.*;
+import akka.http.javadsl.server.*;
+import akka.http.javadsl.server.Directives.*;
 
+//Configuration
 import de.hpi.ddm.configuration.Configuration;
 import de.hpi.ddm.configuration.ConfigurationSingleton;
 
-//TODO Check which are neaded
-import akka.Done;
-import akka.NotUsed;
-import akka.actor.ActorSystem;
-import akka.util.ByteString;
-
-import java.nio.file.Paths;
-import java.math.BigInteger;
-import java.time.Duration;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
-
-import akka.http.javadsl.server.AllDirectives;
-import akka.http.javadsl.server.Route;
-
-import akka.NotUsed;
-import akka.actor.ActorSystem;
-import akka.http.javadsl.ConnectHttp;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.ContentTypes;
-import akka.http.javadsl.model.HttpEntities;
-import akka.http.javadsl.model.HttpEntity.Strict;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.HttpMethods.*;
-import akka.http.javadsl.model.*;
-import akka.http.javadsl.server.AllDirectives;
-import akka.http.javadsl.server.Directives.*;
-import akka.http.javadsl.server.Route;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
-
-import java.io.IOException;
-import akka.japi.function.Function;
-
+//Serialization
+import java.io.Serializable;
+import akka.http.javadsl.unmarshalling.Unmarshaller;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
-import java.io.Serializable;
 
-import akka.actor.AbstractActor;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
+//Additional
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 import scala.concurrent.ExecutionContextExecutor;
-
-import akka.http.javadsl.model.*;
-import akka.http.javadsl.model.headers.*;
-
-import java.util.Optional;
-
-import static akka.pattern.PatternsCS.pipe;
-
-import akka.actor.AbstractActor;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.japi.pf.ReceiveBuilder;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
-import scala.concurrent.ExecutionContextExecutor;
-
-import static akka.pattern.PatternsCS.pipe;
-
-import akka.http.javadsl.unmarshalling.Unmarshaller;
+import akka.japi.function.Function;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
@@ -152,7 +102,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 			oos.writeObject(message);
 			oos.flush();
 			return bos.toByteArray();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			this.log().error("Could not serizalize message");
 			return "IOException occurred - C'est la vie.".getBytes();
 		}
@@ -212,22 +162,14 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 
 		CompletionStage<ServerBinding> serverBindingFuture =
 		serverSource.to(Sink.foreach(connection -> {
-			System.out.println("Accepted new connection from " + connection.remoteAddress());
 			connection.handleWith(Flow.of(HttpRequest.class).map(requestHandler), materializer);
 			}
 		)).run(materializer);
 
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
 		receiverProxy.tell(new BytesMessage<String>("http://"+c.getHost()+":45454/payload", this.sender(), message.getReceiver()), this.self());
 	}
 
 	private void handle(BytesMessage<String> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
 		String url = message.getBytes();
 
 		Materializer materializer = ActorMaterializer.create(this.context().system());
@@ -237,18 +179,12 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		
 		response.whenComplete((content, error) -> {
 			if (error != null) {
-				this.log().error("Oh no! An error occured");
 				this.log().error(error.getMessage());
 			} else {
-				this.log().info("We got a response!");
-				if (content.entity().isStrict()) {
-					this.log().info("Is strict");
-				};
 				CompletionStage<byte[]> stage = Unmarshaller.entityToByteArray().unmarshal(content.entity(),materializer);
 				stage.whenComplete((content2, error2) -> {
 					this.log().info(content2.toString());
 					TransferMessage<?> deserialized = this.getMessageFromBytes(content2);
-					this.log().info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + deserialized.getHost());
 					message.getReceiver().tell(deserialized.getContent(), message.getSender());
 				});
 			}
