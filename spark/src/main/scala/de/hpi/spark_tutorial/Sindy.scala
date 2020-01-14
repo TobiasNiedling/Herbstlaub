446c880390d3915c2d71462df94364ed080642b4
Tobias Niedling <tobiasniedling@localhost.localdomain>
2020-01-14 09:43:15 +0100

    Hello world

--- spark/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ spark/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,8 +1,36 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object Sindy {
+
+  def main(args: Array[String]): Unit = {
+    // Turn off logging
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    // Create a SparkSession to work with Spark
+    val sparkBuilder = SparkSession
+      .builder()
+      .appName("SparkTutorial")
+      .master("local[4]") // local, with 4 worker cores
+    val spark = sparkBuilder.getOrCreate()
+
+    // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
+    spark.conf.set("spark.sql.shuffle.partitions", "8") //
+
+    // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
+    import spark.implicits._
+
+    println("Hello world")
+  }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
